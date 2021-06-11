8cd6069d9ada2da66f1a54b2e3608c9b62fc2681
Hamna Kamran <74135890+hamnakamran10@users.noreply.github.com>
2021-06-11 10:29:19 -0700

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,50 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Ertugrul Theme",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ertugrul happy tune",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Ertugrul Qayi Qabila",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Ertugrul Halime",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Dodurga qabila",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Ertugrul Noyan Fight",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
